f0c7a20d85a1eeef137de5f5868491a3bb83e8ac
Amene <layla_1380@yahoo.com>
2024-04-18 19:54:53 -0500

    form and fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        //console.log(e)
+        const input = document.querySelector('input#searchByID')
+        //console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+
+            .then(res => res.json())
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4')
+                title.textContent = data.title
+                const summary = document.querySelector('section#movieDetails p')
+                summary.textContent = data.summary
+
+            })
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
